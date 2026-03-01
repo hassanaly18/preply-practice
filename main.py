99a2de5a429bfef7c5b023275564c1118335c44c
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2026-03-01 17:00:15 +0500

    conditional amany

---
+++ main.py
@@ -0,0 +1,15 @@
+#conditional statements
+#if, else, elif, match/case
+
+age = 16
+password = "password123"
+
+if password == "password123":
+    print("Logged in")
+else:
+    print("wrong password")
+
+# if age >= 16:
+#     print("You can drive")
+# else:
+#     print("You can NOT drive")
