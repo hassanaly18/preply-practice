b047b21f696809446f3eaf1f63e6c29c2e264259
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2026-03-02 17:49:49 +0500

    conditional 1.2 amany

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 #conditional statements
 #if, else, elif, match/case
 
-age = 16
-password = "password123"
+age = int(input("Enter your age: "))
 
-if password == "password123":
-    print("Logged in")
-else:
-    print("wrong password")
+if age < 13:
+    print("Child")
+elif age <=19:
+    print("teenager")
+
+# writing = 95
+# reading = 56
+
+# if marks >= 90:   
+#     print("A")    
+# if marks >= 80 and marks<90:    
+#     print("B")    
+# if marks>=70 and marks<80:     
+#     print("C")    
+# if marks >= 60 and marks < 70:   
+#     print("D")    
+# else:
+#     print("F")
+
+
+
+
+
+
+
+
+# password = input("Enter password: ")
+
+# if password == "password123":
+#     print("Logged in")
+# else:
+#     print("wrong password")
 
 # if age >= 16:
 #     print("You can drive")
